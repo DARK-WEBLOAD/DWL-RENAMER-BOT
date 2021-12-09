6ca90c35ef5c834df63647030525cb1d4759a7e7
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-09 15:45:40 +0530

    Update __main__.py

--- renamer/__main__.py
+++ renamer/__main__.py
@@ -17,7 +17,7 @@ from pyromod import listen
 
 def main():
 
-    Renamer = Client("Renamer_NsBot",
+    Renamer = Client("DWL_Renamer",
                  bot_token=Config.BOT_TOKEN,
                  api_id=Config.API_ID,
                  api_hash=Config.API_HASH,
