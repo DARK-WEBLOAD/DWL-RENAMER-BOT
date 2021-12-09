3cc7740fb47301e2fc7fdf796afc911cdc0be723
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-09 15:05:20 +0530

    Update __main__.py

--- renamer/__main__.py
+++ renamer/__main__.py
@@ -30,7 +30,7 @@ def main():
     startup_msg = f"Successfully deployed your Renamer at @{me.username}\n"
     startup_msg += f"Pyrogram Version: V{__version__}\n"
     startup_msg += f"Python Version: V{platform.python_version()}\n\n"
-    startup_msg += "Thanks for deploying our bot. Please give a star to my repo and join @universalmovieSS."
+    startup_msg += "Thanks for deploying our bot. Please give a star to my repo and join @cinemahubmoviesS."
     print(startup_msg)
 
     idle()
